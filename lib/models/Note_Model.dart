22404f3e97557c752a16d69ab8b9176bb92d8287
Mahmoud Mohamed <mahmoudmohaamed635@gmail.com>
2026-05-22 05:39:34 +0300

    Create Note model

---
+++ lib/models/Note_Model.dart
@@ -0,0 +1,14 @@
+class NoteModel {
+  final String title;
+  final String subtitle;
+  final String Date;
+  final int color;
+  const NoteModel({
+    required this.color,
+    required this.title,
+    required this.subtitle,
+    required this.Date,
+  });
+
+  delete_Note() {}
+}
